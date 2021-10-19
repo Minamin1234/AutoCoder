c1058ff66cc13bba31ffed08e651d94cba0c0889
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-10-19 16:20:06 +0900

    Add sub windows to origin.
    メインにサブウィンドウを作成しました。

---
+++ AutoCoder/WVarSetting.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoCoder
+{
+    /// <summary>
+    /// WVarSetting.xaml の相互作用ロジック
+    /// </summary>
+    public partial class WVarSetting : Window
+    {
+        public MainWindow WHandler;
+        public WVarSetting()
+        {
+            InitializeComponent();
+        }
+
+        public WVarSetting(MainWindow Handler)
+        {
+            WHandler = Handler;
+            this.Show();
+        }
+    }
+}
---
+++ Test_WPF/Window_Single.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test_WPF
+{
+    /// <summary>
+    /// Window_Single.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Window_Single : Window
+    {
+        public Window_Single()
+        {
+            InitializeComponent();
+        }
+    }
+}
