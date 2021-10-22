908ed95bf4732872a259d94683eab7ae1a240455
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-10-22 18:47:57 +0900

    Creating some code.

--- AutoCoder/WVarSetting.xaml.cs
+++ AutoCoder/WVarSetting.xaml.cs
@@ -12,24 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.Collections.ObjectModel;
+
 namespace AutoCoder
 {
+    public partial class CodeBox
+    {
+        public string Title = "";
+        public string SubTitle = "";
+        public CodeBox(string a,string b)
+        {
+            this.Title = a;
+            this.SubTitle = b;
+        }
+
+        public override string ToString()
+        {
+            return this.Title + "-" + this.SubTitle;
+        }
+    }
     /// <summary>
     /// WVarSetting.xaml の相互作用ロジック
     /// </summary>
     public partial class WVarSetting : Window
     {
         public MainWindow WHandler;
+        public ObservableCollection<CodeBox> CodeList;
         public WVarSetting()
         {
             InitializeComponent();
+            this.Init();
         }
 
         public WVarSetting(MainWindow Handler)
         {
             InitializeComponent();
+            this.Init();
             WHandler = Handler;
             this.Show();
+        }
+
+        public bool Init()
+        {
+            var item = new CodeBox("Setting", "Value");
+            CodeList = new ObservableCollection<CodeBox>();
+            CodeList.Add(item);
+            LB_list.ItemsSource = CodeList;
+
+            return true;
         }
 
         private void Clicked(object sender, RoutedEventArgs e)
