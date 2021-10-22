eed3773f70c787861b8454edf75cb9f1306f29fd
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-10-23 04:25:15 +0900

    Create data classes.

---
+++ AutoCoder/CodeBlockClasses.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCoder
+{
+    public class CodeBlockClass
+    {
+        public CodeBlockClass()
+        {
+
+        }
+    }
+
+    public class UsingBlock : CodeBlockClass
+    {
+        public UsingBlock()
+        {
+
+        }
+    }
+
+    public class ExpressionBlock : CodeBlockClass
+    {
+        public ExpressionBlock()
+        {
+
+        }
+    }
+
+    public class StatementExpBlock : CodeBlockClass
+    {
+        public StatementExpBlock()
+        {
+
+        }
+    }
+
+    public class FunctionBlock : CodeBlockClass
+    {
+        public FunctionBlock()
+        {
+
+        }
+    }
+
+    public class VariableBlock : CodeBlockClass
+    {
+        public VariableBlock()
+        {
+
+        }
+    }
+
+    public class ClassBlock : CodeBlockClass
+    {
+        public ClassBlock()
+        {
+
+        }
+    }
+
+    public class StateBlock : CodeBlockClass
+    {
+        public StateBlock()
+        {
+
+        }
+    }
+
+    public class LoopBlock : CodeBlockClass
+    {
+        public LoopBlock()
+        {
+
+        }
+    }
+}
---
+++ AutoCoder/CodeBoxClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCoder
+{
+    public class CodeBoxClass
+    {
+        public CodeBoxClass()
+        {
+
+        }
+    }
+
+    public class UsingBox : CodeBoxClass
+    {
+        public UsingBox()
+        {
+
+        }
+    }
+
+    public class NamespaceBox : CodeBoxClass
+    {
+        public SourceBox[] Codes;
+        public NamespaceBox()
+        {
+
+        }
+    }
+
+    public class FunctionBox : CodeBoxClass
+    {
+        public FunctionBox()
+        {
+
+        }
+    }
+
+    public class SourceBox : CodeBoxClass
+    {
+        CodeBlockClass[] Codes;
+        public SourceBox()
+        {
+
+        }
+    }
+}
