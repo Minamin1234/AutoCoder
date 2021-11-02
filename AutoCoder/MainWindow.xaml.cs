c63d1a3fa7bcbb5bbcce2b9608513d5b36f27e58
Minamin <62072288+Minamin1234@users.noreply.github.com>
2021-11-02 18:03:50 +0900

    Testing comboBox.

--- AutoCoder/MainWindow.xaml.cs
+++ AutoCoder/MainWindow.xaml.cs
@@ -20,9 +20,27 @@ namespace AutoCoder
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ComboBoxItem[] list = {new ComboBoxItem()};
         public MainWindow()
         {
             InitializeComponent();
+            
+            for(int i = 0;i < 10;i++)
+            {
+                Array.Resize(ref list, list.Length + 1);
+                this.list[i] = new ComboBoxItem();
+                this.list[i].Content = "A";
+            }
+            this.Add();
+        }
+
+        public void Add()
+        {
+            foreach(ComboBoxItem CBI in this.list)
+            {
+                Console.WriteLine(CBI);
+                this.CB_namespace.Items.Add(CBI);
+            }
         }
     }
 }
