e5c761a17d066793b6b774ff514eb39e7bc5b8b0
Minamin <minamin37345@gmail.com>
2022-01-04 02:25:46 +0900

    エラーハンドラーの追加

---
+++ AutoCoder/Components/ErrorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCoder
+{
+    public class ErrorHandler
+    {
+        protected List<string> Errors = new List<string>();
+        public ErrorHandler() { }
+        public void AddError(Exception error)
+        {
+
+        }
+        public void ExportLog()
+        {
+
+        }
+    }
+}
