e900223a42e63584986ae74bf78409d7ef796c01
Minamin <62072288+Minamin1234@users.noreply.github.com>
2022-01-11 15:23:01 +0900

    変数の作成。クラスデータマネージャーウィンドウの関数の宣言。イベント実装。

--- AutoCoder/ClassManagerWindow.xaml.cs
+++ AutoCoder/ClassManagerWindow.xaml.cs
@@ -17,11 +17,87 @@ namespace AutoCoder
     /// <summary>
     /// ClassManagerWindow.xaml の相互作用ロジック
     /// </summary>
-    public partial class ClassManagerWindow : Window
+    public partial class ClassManagerWindow : Window,IDataEditing
     {
+        public MainWindow WHandler = null;
+        public Namespace CurrentFile = null;
+        protected Window SubWindow = null;
         public ClassManagerWindow()
         {
             InitializeComponent();
+        }
+
+        public ClassManagerWindow(Namespace TargetFile,MainWindow whandler)
+        {
+            InitializeComponent();
+            if (TargetFile == null || whandler == null) throw new ArgumentNullException();
+            this.WHandler = whandler;
+            this.CurrentFile = TargetFile;
+            this.Initialize();
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        void IDataEditing.SetSubWindow(Window nwindow)
+        {
+
+        }
+
+        void IDataEditing.ClearSubWindow()
+        {
+
+        }
+
+        void IDataEditing.CommitNewData(ACObject newData)
+        {
+
+        }
+
+        void IDataEditing.FinishedEditingData()
+        {
+
+        }
+
+        void IDataEditing.OpenCreateWindow()
+        {
+
+        }
+
+        void IDataEditing.OpenCreateWindow(ACObject targetdata)
+        {
+
+        }
+
+        public void ReloadListData()
+        {
+
+        }
+
+        private void B_Clicked(object sender, RoutedEventArgs e)
+        {
+            var CurrentButton = (Button)sender;
+            var iself = (IDataEditing)this;
+
+            if(CurrentButton.Name == this.B_add.Name)
+            {
+
+            }
+            else if(CurrentButton.Name == this.B_edit.Name)
+            {
+
+            }
+            else if(CurrentButton.Name == this.B_delete.Name)
+            {
+
+            }
+        }
+
+        private void WClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
