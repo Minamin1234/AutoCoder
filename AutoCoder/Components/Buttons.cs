cb0bd4ca90dbf3481543c350caf07aea317148ee
Minamin <minamin37345@gmail.com>
2022-06-12 23:16:03 +0900

    レイアウトの変更

---
+++ AutoCoder/Components/Buttons.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoCoder
+{
+    public partial class B_NewProject : Button
+    {
+        public B_NewProject()
+        {
+            this.Content = "New Project";
+            this.HorizontalAlignment = HorizontalAlignment.Left;
+            this.VerticalAlignment = VerticalAlignment.Top;
+        }
+    }
+
+    public partial class B_LoadProject : Button
+    {
+        public B_LoadProject()
+        {
+            this.Content = "Load Project";
+            this.HorizontalAlignment = HorizontalAlignment.Left;
+            this.VerticalAlignment = VerticalAlignment.Top;
+        }
+    }
+}
